a6059bc3efdf5ee62a5a34d89a0d981a34dea4e9
Yomisawa <130817740+Yomisawa@users.noreply.github.com>
2024-03-09 13:14:00 -0600

    Add files via upload

---
+++ problem/proble1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+
+namespace Programa1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Bienvenido al generador de correos!");
+            Console.Write("Ingresa tu correo electrónico de dominio Gmail: ");
+            string inputEmail = Console.ReadLine();
+
+            if (inputEmail.EndsWith("@gmail.com"))
+            {
+                // Reemplazar el dominio "gmail.com" por "miumg.edu.gt"
+                string newEmail = inputEmail.Replace("@gmail.com", "@miumg.edu.gt");
+                Console.WriteLine("Tu nuevo correo es" + newEmail);
+            }
+            else
+            {
+                Console.WriteLine("El correo ingresado no es de dominio Gmail.");
+            } Console.Read();
+        }
+    }
+}
---
+++ problem/proble2.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Programa2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("¡Bienvenido al calculador de descuentos!");
+
+            double totalSinDescuento = 0;
+            double totalConDescuento = 0;
+
+            for (int i = 1; i <= 3; i++)
+            {
+                Console.Write("Ingresa el precio del producto {i}: ");
+                double precio = Convert.ToDouble(Console.ReadLine());
+
+                if (precio > 100)
+                {
+                    // Aplicar 15% de descuento
+                    double descuento = precio * 0.15;
+                    totalConDescuento += precio - descuento;
+                }
+                else
+                {
+                    totalConDescuento += precio;
+                }
+
+                totalSinDescuento += precio;
+            }
+
+            double ahorro = totalSinDescuento - totalConDescuento;
+            Console.WriteLine("Total de compra sin descuento:"+ totalSinDescuento);
+            Console.WriteLine("Total de compra con descuento"+ totalConDescuento);
+            Console.WriteLine("Ahorro total:" + ahorro);
+            Console.Read();
+        }
+        
+    }
+}
+
+
+
+
